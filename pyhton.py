ec829fb8013265b707043e9b958a806591f751e2
karan <karansutharkaransuthar55@gmail.com>
2023-05-04 23:57:24 -0400

    python rename files

---
+++ pyhton.py
@@ -0,0 +1,31 @@
+import random  
+import string
+st = input("Enter message:")
+words = st.split(" ")
+coding = input("1 for Coding or 0 for Decoding")
+coding = True if (coding=="1") else False
+print(coding)
+if(coding):
+  nwords = []
+  for word in words:
+    if(len(word)>=3):
+      
+      stnew =  word[1:] + word[0]
+      code = ''.join(random.choices(string.ascii_lowercase)) + stnew+ ''.join(random.choices(string.ascii_lowercase))
+      nwords.append(code)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
+
+else:
+  nwords = []
+  for word in words:
+    if(len(word)>=3): 
+      stnew = word[1:-1]
+      stnew = stnew[-1] + stnew[:-1]
+      nwords.append(stnew)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
+  
+  
